31805af15866a2bba0950fe10a40cdd3e638acc4
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-04-07 15:05:44 +0200

    connecting to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MangoDb is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
