7e4ab6e7ef27a0d031ef897e9eff0b7e119a09e6
TonyBlaiseNTAHE <ntahetonyblaise@gmail.com>
2024-04-07 09:51:23 +0200

    creating the header

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Button } from "flowbite-react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
